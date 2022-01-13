de3f7bcea4afede755b70b0b640c22e70296f42f
ambar gupta <ambar.gtm@gmail.com>
2022-01-13 23:16:07 +0530

    adding dynamic public key genration feature

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # springSecurity_AuthorizationServer
  this is  project for the Authorization Server which generate JWt based OAuth Token  which is further use for the Resource server to access the resource 
+in this release we are adding the feature of to generate the dynamic public key to utilise the rest service of Authorization server
--- src/main/java/com/learningpath/authorizationserver/security/configuration/AuthorizationServerConfig.java
+++ src/main/java/com/learningpath/authorizationserver/security/configuration/AuthorizationServerConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -53,6 +54,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         clients.inMemory().withClient("couponclientapp").secret(passwordEncoder.encode("9999"))
                 .authorizedGrantTypes("password","refresh_token").scopes("read","write").resourceIds(RESOURCE_ID);
     }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()");
+    }
+
     @Bean
     public TokenStore tokenStore()
     {
